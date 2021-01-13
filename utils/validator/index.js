7fa698dc45758612d6b566378aafdbe4bcfc57a7
edhymx1 <edhy.mxm@gmail.com>
2021-01-13 16:01:44 -0600

    refactor validatorjs

--- utils/validator/index.js
+++ utils/validator/index.js
@@ -1,41 +1,41 @@
 const Validator = require('validatorjs');
 
 Validator.register(
-    'price',
-    function (value, requirement, attribute) {
-        return value.toString().match(/^\d+(.\d{2})?$/);
-    },
-    'the value is not a valid format'
+  'price',
+  function (value, requirement, attribute) {
+    return value.toString().match(/^\d+(.\d{2})?$/);
+  },
+  'the value is not a valid format'
 );
 
 Validator.register(
-    'id',
-    (value, requirement, attribute) => {
-        return value.toString().match(/^[1-9]\d*$/);
-    },
-    'the value is not a valid format'
+  'id',
+  (value, requirement, attribute) => {
+    return value.toString().match(/^[1-9]\d*$/);
+  },
+  'the value is not a valid format'
 );
 
 Validator.register(
-    'picture',
-    (value, requirement, attribute) => {
-        return value.toString().match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-    },
-    'the value is not a valid format'
+  'picture',
+  (value, requirement, attribute) => {
+    return value.toString().match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+  },
+  'the value is not a valid format'
 );
 
 Validator.register(
-    'stock',
-    (value, requirement, attribute) => {
-        return value.toString().match(/^\d+$/);
-    },
-    'the value is not a valid format'
+  'stock',
+  (value, requirement, attribute) => {
+    return value.toString().match(/^\d+$/);
+  },
+  'the value is not a valid format'
 );
 
 Validator.register(
-    'discount',
-    (value, requirement, attribute) => {
-        return value.toString().match(/^(\d(\d)?|100)$/);
-    },
-    'the value is not a valid format'
+  'discount',
+  (value, requirement, attribute) => {
+    return value.toString().match(/^(\d(\d)?|100)$/);
+  },
+  'the value is not a valid format'
 );
